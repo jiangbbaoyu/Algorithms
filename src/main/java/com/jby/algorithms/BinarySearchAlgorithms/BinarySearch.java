d1dd56aecb4cc3c4ae042f506aee9771b379e695
lcr2 <lcr@se27.com>
2022-01-22 14:10:57 +0800

    find a local max number in sorted array

--- src/main/java/com/jby/algorithms/BinarySearchAlgorithms/BinarySearch.java
+++ src/main/java/com/jby/algorithms/BinarySearchAlgorithms/BinarySearch.java
@@ -6,7 +6,12 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
- * 在一个有序数组中，找某个数是否存在, 二分查找， O(logN)
+ * 二分查找， O(logN)
+ * 二分法能解决四种问题：
+ *  （1）有序数组查找某个值是否存在
+ *  （2）有序数组查找大于等于某个值的最左侧
+ *  （3）有序数组查找小于等于某个值的最右侧
+ *  （4）局部最值问题
  *
  */
 public class BinarySearch {
@@ -18,9 +23,9 @@ public class BinarySearch {
         // 考虑到二分到数组只剩一个元素的情况，此处要加上等于号。
         // 否则如果此时left+1==right, 且数组中right位置的值正好是target的话。
         // 本轮计算出的middle值为left, 而arr[middle]<target; 下一轮中left就赋值为middle+1 ,与right值一致。如果没有等于号的话，就会导致错过target在right位置的情况
-        while (left<right){
+        while (left<=right){
             int middle = (left + right) / 2;
-            System.out.println("middle:"+middle);
+//            System.out.println("middle:"+middle);
             if (arr[middle]==target){
                 return middle;
             }
